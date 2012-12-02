0a2d4dd9e99067afce49f7740f92482483282233
Alexander <vertex0@hotmail.com>
2012-12-02 17:58:27 +0400

    Добавлен текстовый редактор
    Текстому редактору все еще необходимо: установить перед работой поле
    protectedObjects, вызвать setUserName(QString). Не реализована проверка
    доступа при открытии, сохранении, сохранении как и т.д. (cмотри
    комментарии XXX)

--- textedit.h
+++ textedit.h
@@ -2,6 +2,8 @@
 #define TEXTEDIT_H
 
 #include <QMainWindow>
+#include "protectedobjects.h"
+#include "newfile.h"
 
 namespace Ui {
     class TextEdit;
@@ -11,13 +13,29 @@ class TextEdit : public QMainWindow
 {
     Q_OBJECT
 
+public slots:
+    void saveFileSlot();
+    void openFileSlot();
+    void saveFileAsSlot();
+    void newFileSlot();
+
+
 public:
+    ProtectedObjects *protObj; //someone should initialize this value
     explicit TextEdit(QWidget *parent = 0);
     ~TextEdit();
     void setUserName(QString name);
     void open(QString path);
+    void newSession();
 private:
     Ui::TextEdit *ui;
+
+    void addFileToPassports();
+    NewFile* newFileDialog; //dialog for creating file from the editor.
+    QString currentFilePath;
+    QString currentUser;
 };
 
 #endif // TEXTEDIT_H
+
+
